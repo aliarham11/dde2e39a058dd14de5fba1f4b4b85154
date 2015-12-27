60ab846dfc49da987681cdc208b14d022b491d82
aliarham11 <aliarham.skom@gmail.com>
2015-12-27 23:17:06 +0700

    fix typo

--- assets/js/pages/interface3.js
+++ assets/js/pages/interface3.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('#interface3-btn-nav').click(function(){
         $('#interface3-story1').css('display', 'none');
 
-        $('#interface3-header').text('Komponen Biaya Pembentuk Harga Baru').append('<i class="material-icons left">subtitles</i>');
+        $('#interface3-header').text('Komponen Biaya Pembentuk Harga Kapal Baru').append('<i class="material-icons left">subtitles</i>');
         $('#interface3-title').css('display','none');
         $('#interface3-chart').css('display','block');
         get_interface3_chart();
