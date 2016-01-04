5a48e53ee4bdc077f26b70b7d57bef0a36497778
galihputera <galih.putera@bukalapak.com>
2016-01-04 10:58:24 +0700

    show correct answr

--- application/views/quiz_cost/index.php
+++ application/views/quiz_cost/index.php
@@ -6,7 +6,7 @@
 ?>
 
 <section id="container">
- 
+
   <div class="container z-depth-3">
     <nav>
       <div class="nav-wrapper blue darken-3">
@@ -14,7 +14,7 @@
           Quis Tebak Cost<i class="material-icons left">perm_identity</i>
         </a>
       </div>
-    </nav>  
+    </nav>
     <div class="grey lighten-5">
         <div class="row">
           <div class="col s12">
@@ -56,7 +56,20 @@
                 <thead>
                   <tr>
                     <td>COST BREAKDOWN</td>
-                    <td>US$</td>
+                    <?php
+                      if ($question->score_cost == null){
+                    ?>
+                        <td>US$</td>
+                    <?php
+                      }
+                      else{
+                    ?>
+                        <td>Answer US$</td>
+                        <td>Actual US$</td>
+                        <td>Correct</td>
+                    <?php
+                      }
+                    ?>
                   </tr>
                 </thead>
                 <tbody>
@@ -65,15 +78,44 @@
                   ?>
                       <tr>
                         <td><?php echo $answer->name; ?></td>
-                        <td>
-                         <input type="text" name="estimate[<?php echo $answer->part_id;?>]" value="<?php echo $answer->estimate; ?>"/>
-                        </td>
+                        <?php
+                          if ($question->score_cost == null){
+                            if ($answer->actual != null)
+                            {
+                        ?>
+                            <td>
+                              <input type="text" name="estimate[<?php echo $answer->part_id;?>]" value="<?php echo $answer->estimate; ?>"/>
+                            </td>
+                        <?php
+                            }
+                          }
+                          else{
+                        ?>
+                            <td><?php echo $answer->estimate;?></td>
+                            <td><?php echo $answer->actual;?></td>
+                            <td>
+                                <?php
+                                  $margin = $answer->actual * $margin_percentage / 100;
+                                  $diff = $answer->actual - $answer->estimate;
+                                  if(abs($diff) < $margin || $answer->actual == null){
+                                    echo "Benar";
+                                  }
+                                  else echo "Salah";
+                                ?>
+                            </td>
+                        <?php
+                          }
+                        ?>
                       </tr>
                   <?php
                     }
                   ?>
                 </tbody>
               </table>
+              <?php
+                if ($question->score_cost == null)
+                {
+              ?>
               <p class="center-align">
                 <input name="save" type="radio" value="0" checked="true" id="0"/>
                 <label for="0">Simpan</label>
@@ -82,15 +124,31 @@
                 <input name="save" type="radio" value="1" id="1"/>
                 <label for="1">Simpan dan Selesai</label>
               </p>
+              <?php
+                }
+              ?>
               <div class="input-field center-align" style="margin-bottom:30px">
-                <button class="btn waves-effect waves-light blue darken-3" type="submit" name="action" id="next_question">Kirim Jawaban
-                  <i class="material-icons right">send</i>
-                </button>
-              </div>
-            </form>  
+                <?php
+                  if ($question->score_cost == null)
+                  {
+                ?>
+                    <button class="btn waves-effect waves-light blue darken-3" type="submit" name="action" id="next_question">Kirim Jawaban
+                      <i class="material-icons right">send</i>
+                    </button>
+                <?php
+                  }
+                  else
+                  {
+                ?>
+                    <a class="btn waves-effect waves-light blue darken-3" href="<?php echo base_url()."games/run";?>"> Next</a>
+                <?php
+                  }
+                ?>
+                  </div>
+            </form>
           </div>
         </div>
-      
+
     </div>
   </div>
 
